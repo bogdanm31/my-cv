6b88ee94e2084eb34f66d1df2c0f1fb30f7e8c0f
bogdan.oprea <bogdan.oprea@pwrr.expert>
2025-02-25 18:21:15 +0200

    updated head

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -3,7 +3,7 @@ import Section from "./ui/section";
 const Intro = () => {
   return (
     <Section title="Summary">
-      I am a frontend developer with strong focus on creating intuitive and visually appealing user interfaces, passionate about optimizing performance and ensuring cross-browser compatibility. With a blend of creativity and analytical skills, I am detail-oriented and driven by desire to solve complex problems, delivering high quality results.
+      I am a developer with strong focus on creating intuitive and visually appealing user interfaces, passionate about optimizing performance and ensuring cross-browser compatibility. With a blend of creativity and analytical skills, I am detail-oriented and driven by desire to solve complex problems, delivering high quality results.
     </Section>
   );
 };
